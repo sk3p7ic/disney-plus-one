73c6a12f02a5b1692bcfb72a9eb9325c320f6417
Joshua Ibrom <joshuaibrom@gmail.com>
2023-01-21 09:14:45 -0600

    Installed ApolloClient for Graphql

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { App } from "./App";
+
+const client = new ApolloClient({
+  uri: "https://api.disneyapi.dev/graphql",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
