d3a3fc918a40c8dacaf464e14be2bf829aec1da2
Joshua Ibrom <joshuaibrom@gmail.com>
2023-01-22 19:06:53 -0600

    Changed method to toggle if character in favorites

--- src/contexts/FavoritesContext.tsx
+++ src/contexts/FavoritesContext.tsx
@@ -2,12 +2,12 @@ import React, { useContext, useState } from "react";
 
 type FavoritesContextValue = {
   favorites: Set<number>;
-  addFavorites: (characterId: number) => void;
+  toggleFavorite: (characterId: number) => void;
 };
 
 const FavoritesContext = React.createContext<FavoritesContextValue>({
   favorites: new Set(),
-  addFavorites: (_: number) => {},
+  toggleFavorite: (_: number) => {},
 });
 
 export const useFavorites = () => useContext(FavoritesContext);
@@ -21,14 +21,15 @@ export const FavoritesContextProvider = ({
 }: FavoritesContextProviderProps) => {
   const [favorites, setFavorites] = useState<Set<number>>(new Set());
 
-  const addFavorites = (characterId: number) => {
+  const toggleFavorite = (characterId: number) => {
     const newFavorites = new Set(favorites);
-    newFavorites.add(characterId);
+    if (newFavorites.has(characterId)) newFavorites.delete(characterId);
+    else newFavorites.add(characterId);
     setFavorites(newFavorites);
   };
 
   return (
-    <FavoritesContext.Provider value={{ favorites, addFavorites }}>
+    <FavoritesContext.Provider value={{ favorites, toggleFavorite }}>
       {children}
     </FavoritesContext.Provider>
   );
